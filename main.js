856677e830ba057a02945e6b3625f6e72633be2f
Ajju2211 <azharuddinm2211@gmail.com>
2020-08-03 00:10:37 +0530

    emojis added

--- main.js
+++ main.js
@@ -17,18 +17,26 @@ $(document).ready(function(){
 
 //on send button
 $(document).keydown(function (e) { 
-    if(e.keyCode===13){
-        const userMsg = $(".Input_field").val();
-        console.log(userMsg);
-        // // $(".Messages_list").text();
-        // $(".Messages_list").text(userMsg);
-        let Messages_list = `<div class="Messages_list">
-        <div class="Message_text_container">
-          <img src="./naaniz.png" style="width: 30px;height: 30px;">
-          <p>${userMsg}</p>
-          <span class="time-right">11:00</span>
-        </div>`;
-        $(".Messages").append(Messages_list);
+    
+    if(e.keyCode===13 && !e.shiftKey){
+         
+            const userMsg = $(".emoji-wysiwyg-editor.Input_field.parent-has-scroll").text();
+            if(userMsg.replace(/\s/g, '').length){
+                            console.log(userMsg);
+            $(".emoji-wysiwyg-editor.Input_field.parent-has-scroll").html("");
+
+            let Messages_list = `<div class="Messages_list">
+            <div class="Message_text_container">
+              <img src="./naaniz.png" style="width: 30px;height: 30px;">
+              <p>${userMsg}</p>
+              <span class="time-right">11:00</span>
+            </div>`;
+            $(".Messages").append(Messages_list);
+            document.querySelector(".Messages").scrollTop = document.querySelector(".Messages").scrollHeight;
+            }
+
+
+
     }
     
 });
